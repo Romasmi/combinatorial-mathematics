45f48a14010344b6baae319ad53389c1d4d6c2b4
romasmi <romasmi@ro.ru>
2019-02-16 20:52:05 +0300

    Add task_1.5

---
+++ lab_1/task_1.5/pch.h
@@ -0,0 +1,18 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <algorithm>
+#include <boost/timer.hpp>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <stdio.h>
+#include <tchar.h>
+#include <utility>
+#include <vector>
+#include <string>
+
+#endif
+
+#pragma once
